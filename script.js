284629eaaf8ea73bf347bbbbf37498cb63d2aba4
Matanski <matangrod@outlook.co.il>
2023-06-05 12:32:05 +0300

    mobile

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-const text=`A confirmation email has been sent to ${email}. Please open it and click the button inside to confirm your subscription`;
+const words = new URLSearchParams(window.location.search);
+const emailValue= words.get('email');
+
+console.log(emailValue);
+
+const story = `<p>A <span class="word" title="id:email">${emailValue}</span> </p>`;
+const textNode = document.createTextNode("Hello World");
+
+
+document.getElementById('confirm').innerHTML = `A confirmation email has been sent to <span style=font-weight:bold>${emailValue}</span>. please open it and click the button inside to confirm your subscription.`;
+
+
+document.getElementById('dismiss').addEventListener(onclick,function(){
+    document.location.href ='index.html';
+});
